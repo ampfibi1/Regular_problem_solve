0a1103bd0148533c723376c80bc341ed172c97b8
tamjid666 <168060724+tamjid666@users.noreply.github.com>
2024-10-12 01:13:46 +0600

    Add files via upload

---
+++ A_make_all_equal.cpp
@@ -0,0 +1,23 @@
+//https://codeforces.com/contest/2001/problem/A
+#include<bits/stdc++.h>
+using namespace std; 
+ 
+void solve()
+{
+	int n ; cin >> n ; 
+	map<int,int> m ; 
+	for(int i=0; i<n; ++i)
+	{
+		int x ; cin >> x ; 
+		m[x]++;
+	}
+	int mx = 0 ; 
+	for(auto x:m)
+		mx = max(mx,x.second);
+	cout << n-mx << endl ; 
+}
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0);
+	int t ; cin >> t ; while(t--)solve();
+}
