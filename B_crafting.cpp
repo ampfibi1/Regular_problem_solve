b82f6374c803974e354dd19661118dc282d97203
abdullah al tamjid <abdullahtamjid676@gmail.com>
2025-01-16 21:15:28 +0600

    greddy_sorting ,constructive algorithm

---
+++ B_crafting.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+void solve(){
+	int n ; cin >> n ; int a[n] , b[n] ; 
+
+	for(int &x:a) cin >> x ; 
+	for(int &x:b) cin >> x ; 
+
+	for(int i=0; i<n; ++i){
+		if(a[i]<b[i]){
+			int spend = b[i]-a[i] ;
+			// cout << spend << endl ; 
+			for(int j=0; j<n; ++j){
+				if(i==j) {a[i]+=spend;continue;}
+				a[j] -= spend;
+			}
+			break;
+		}
+	}
+
+	bool ok = true ; 
+	for(int i=0; i<n; ++i){
+		if(a[i]<b[i]) ok = false ; 
+	}
+	cout << (ok?"YES":"NO") << endl ; 
+}
+int main(){
+	int t ;cin >> t ; while(t--) solve();
+	return 0 ; 
+}
